44f757429507202b201815a68cae8164b9021828
Philipp Schiffmann <philippschiffmann93@gmail.com>
2022-06-06 22:30:16 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    hmr: false,
+  },
+  optimizeDeps: {
+    // Workaround for https://github.com/vitejs/vite/issues/6215
+    include: ["react/jsx-runtime"],
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // https://github.com/vitejs/vite/issues/905#issuecomment-708523523
+        includePaths: ["../"],
+      },
+    },
+  },
+});
